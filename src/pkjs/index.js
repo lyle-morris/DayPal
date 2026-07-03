daaad3d37e79aed40cfbd83995b132f3972d21a9
Lyle Morris <lylemorris@pm.me>
2026-07-03 13:51:03 -0400

    Point config URL to hosted DayMate config repo for testing

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,4 +1,4 @@
-var CONFIG_URL = 'https://lyle-morris.github.io/DayPal-config/';
+var CONFIG_URL = 'https://lyle-morris.github.io/DayMate-config/';
 var DONATION_URL = 'https://ko-fi.com/lylemorris';
 var SETTINGS_KEY = 'daypal_settings';
 var LEGACY_SETTINGS_KEY = 'daymate_settings';
